023f656180d3a64de32cc9652cea51562151e3f5
Gaurav Balyan <gaurav.balyan24@gmail.com>
2020-04-01 17:40:16 +0530

    Changed folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { AgGridModule } from 'ag-grid-angular';
+import { PrimengModule } from './primeng/primeng/primeng.module';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AgGridModule.withComponents([]),
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    PrimengModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
